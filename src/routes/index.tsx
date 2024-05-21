e2355ece8e00392e2f97771b081a5000bc8f98b5
Gaurang Ratnaparkhi <68043860+gnaaruag@users.noreply.github.com>
2024-05-21 16:38:19 +0530

    add signup and signin

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,22 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 import Notfound from "../pages/Notfound";
+import Signin from "../pages/signin";
+import Signup from "../pages/signup";
 
 const router = createBrowserRouter([
   {
+    path: "/signin",
+    element: <Signin />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
     path: "*",
-    element: <Notfound />
-  }
+    element: <Notfound />,
+  },
 ]);
 
 export default router;
