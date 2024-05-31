545bcca9fc2fc90abefbe1f133b68127db9e97a4
Gaurang Ratnaparkhi <68043860+gnaaruag@users.noreply.github.com>
2024-06-01 04:03:32 +0530

    add filter by sport tab to newslist

--- src/pages/news/NewsList.tsx
+++ src/pages/news/NewsList.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { useArticlesState } from "../../context/articles/context";
 import ArticleId from "./NewsItem";
-import "./list.css"
+import "./list.css";
 
 export default function ArticleList() {
   const state = useArticlesState();
   const { articles, isLoading, isError, errorMessage } = state;
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const categories = [
+    "All",
+    "Basketball",
+    "American Football",
+    "Rugby",
+    "Field Hockey",
+    "Table Tennis",
+    "Cricket"
+  ];
+
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const filteredArticles = selectedCategory === "All" 
+    ? articles 
+    : articles.filter(article => article.sport.name.toLowerCase() === selectedCategory.toLowerCase());
 
   if (isLoading) {
     return <span>Loading...</span>;
@@ -16,10 +35,23 @@ export default function ArticleList() {
   }
 
   return (
-    <div className="flex flex-col gap-2 overflow-y-scroll list">
-      {articles.map((article: any) => {
-        return <ArticleId key={article.id} id={article.id} />;
-      })}
+    <div>
+      <div className="category-buttons flex gap-4 my-4">
+        {categories.map(category => (
+          <button 
+            key={category} 
+            onClick={() => handleCategoryChange(category)}
+            className={category === selectedCategory ? "active bg-slate-500 hover:bg-gray-400 dark:bg-blue-500 p-2 rounded-md hover:bg-blue-400" : "p-2 rounded-md bg-slate-300 hover:bg-gray-400 dark:hover:bg-blue-400 bg-slate-800"}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
+      <div className="flex flex-col gap-2 overflow-y-scroll list">
+        {filteredArticles.map((article) => (
+          <ArticleId key={article.id} id={article.id} />
+        ))}
+      </div>
     </div>
   );
 }
